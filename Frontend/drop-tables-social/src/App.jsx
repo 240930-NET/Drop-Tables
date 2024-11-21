841111505a30eb34ffb6cd9774ef2f9c7313b78e
anderja2000 <70916539+anderja2000@users.noreply.github.com>
2024-11-20 21:16:44 -0600

    addded some styles can change

--- Frontend/drop-tables-social/src/App.jsx
+++ Frontend/drop-tables-social/src/App.jsx
@@ -8,26 +8,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='content'>
+        <h1>Drop Tables</h1>
+        <ul>
+          <li><a href="/api/users">Users</a></li>
+          <li><a href="/api/posts">Posts</a></li>
+          <li><a href="/api/follows">Follows</a></li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
