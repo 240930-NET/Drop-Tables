f52ecf082dd1c4a9fc08ca2d90988f2cac6b7378
Elton V <43376123+eltonvilla@users.noreply.github.com>
2024-11-26 22:26:36 -0700

    Finished profile page

--- Frontend/drop-tables-social/src/pages/ProfilePage.jsx
+++ Frontend/drop-tables-social/src/pages/ProfilePage.jsx
@@ -1,21 +1,30 @@
-import { useContext, useState } from "react"
+import { useContext, useState, useEffect } from "react"
 import Profile from "../components/Profile";
 import ProfileTab from "../components/ProfileTab";
-import PostTile from "../components/PostTile";
 import Post from "../components/Post";
 import { UserContext } from "../context/UserContext";
+import { getUsers } from "../functions/user"
 import "../Styles/ProfilePage.css"
 import "../Styles/Home.css"
 
 const ProfilePage = () => {
     const [tab, setTab] = useState("posts");
     const { currentUser } = useContext(UserContext);
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+          const userIds = currentUser.likes.map((user) => user.userId);
+          const users = await getUsers(userIds);
+          setUsers(users);
+        };
+    
+        fetchPosts();
+    }, [currentUser]);
 
     const handleTabChange = (selectedTab) => {
         setTab(selectedTab);
     }
-
-    console.log(currentUser);
     return (
         <div className="profile-page">
             <Profile />
@@ -29,11 +38,14 @@ const ProfilePage = () => {
                     ))
                 }
                 {tab === "likes" && 
-                    currentUser?.likes?.map((like, index) => (
-                        <div key={post.postId}>
-                            <Post postId = {post.postId} user = {null} content ={post.content}/>
-                        </div>
-                    ))    
+                     currentUser.likes.map((post) => {
+                        const user = users?.find((u) => u.userId === post.userId);
+                        return user ? (
+                          <div key={post.postId} id="post-list">
+                            <Post postId = {post.postId} user = {user} content ={post.content}/>
+                          </div>
+                        ) : null;
+                      })
                 }
             </div>
         </div>
