90d456a94111fe7f0bf995e540bf9ac390071dd6
RylanMattocks <rmattoc@wgu.edu>
2024-11-27 00:50:57 -0500

    changing localhost to hosted api

--- Frontend/drop-tables-social/src/functions/post.js
+++ Frontend/drop-tables-social/src/functions/post.js
@@ -1,4 +1,4 @@
-const url = 'http://localhost:5001/api/Post/';
+const url = 'https://droptables.azurewebsites.net/api/Post/';
 
 export const createPost = async (userId, content, imageURL) => {
     try {
