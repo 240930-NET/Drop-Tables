c20bc430021233d81856eab2c340f3bb923e9c4f
Gorawto41 <Gorawto42@yahoo.com>
2024-11-26 19:20:56 -0600

    Fixed navbar allignment

--- Frontend/drop-tables-social/src/components/Navbar.jsx
+++ Frontend/drop-tables-social/src/components/Navbar.jsx
@@ -20,25 +20,36 @@ function Navbar() {
     return (
         <>
             <nav>
+                <section id="hamburger" onClick={toggleMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </section>
+                <div id="logo">
+                    <div className="bookIcon">
+                        <div className="bookLabel" />
+                    </div>
+                    <NavLink id="dropTable" to="/home">DROP TABLE</NavLink>
+                </div>
                 <ul id="navList">
-                    <li id="logo">
-                        <div className="bookIcon">
-                            <div className="bookLabel" />
-                        </div>
-                        <NavLink id="dropTable" to="/home">DROP TABLE</NavLink>
+                    <li id="navButtonsItem">
+                        <section id="navButtons">
+                            <ul>
+                                <li className="navButton">
+                                    <NavLink to="/home">Home</NavLink>
+                                </li>
+                                <li className="navButton">
+                                    <NavLink to="/feed">Your Feed</NavLink>
+                                </li>
+                                <li id="profileButton" className="navButton">
+                                    <NavLink to="/profile">Profile</NavLink>
+                                </li>
+                            </ul>
+                        </section>
                     </li>
-                    <section id="navButtons">
-                        <li className="navButton">
-                            <NavLink to="/home">Home</NavLink>
-                        </li>
-                        <li className="navButton">
-                            <NavLink to="/feed">Your Feed</NavLink>
-                        </li>
-                        <li id="profileButton" className="navButton">
-                            <NavLink to="/profile">Profile</NavLink>
-                        </li>
-                    </section>
-                    <section id="avatarSection">
+                </ul>
+                <section id="avatarSection">
+                    <ul>
                         <li>
                             <button onClick={logout}>Logout</button>
                         </li>
@@ -49,12 +60,7 @@ function Navbar() {
                                 id="avatar" 
                             />
                         </li>
-                    </section>
-                </ul>
-                <section id="hamburger" onClick={toggleMenu}>
-                    <span></span>
-                    <span></span>
-                    <span></span>
+                    </ul>
                 </section>
             </nav>
             {/* Sidebar should be placed here */}
